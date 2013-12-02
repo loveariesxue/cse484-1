385d81a5e18765c1174af26a830c4843d0b1c81a
root <root@joesonw-Ubuntu.(none)>
2013-12-02 04:47:24 -0500

    build query for sift extracted key points

--- Makefile
+++ Makefile
@@ -1,2 +1,11 @@
-search: main.cpp 
-	g++ -std=c++0x main.cpp -o search -I ./flann/headers ./flann/libs/linux/libflann_x64.a -lstdc++
+#LUCENE_PATH=lucene-4.6.0/
+#LUCENE_CLASS_PATH=$(LUCENE_PATH)/core/lucene-core-4.6.0.jar:$(LUCENE_PATH)/queryparser/lucene-query/lucene-queryparser-4.6.0.jar:$(LUCENE_PATH)/analysis/common/lucene-analyzers-common-4.6.0.jar
+all:build-trec build-new
+
+
+build-index: IndexTREC.java TrecDocIterator.java
+	javac -classpath $(LUCENE_CLASS_PATH) TrecDocIterator.java IndexTREC.java
+build-trec: main.cpp 
+	g++ -std=c++0x main.cpp -o build-trec -I ./flann/headers ./flann/libs/linux/libflann_x64.a -lstdc++
+build-new: build-new.cpp
+	g++ -std=c++0x build-new.cpp -o build-new -I ./flann/headers ./flann/libs/linux/libflann_x64.a -lstdc++
---
+++ build-new.cpp
@@ -0,0 +1,73 @@
+#include "flann/headers/flann.h"
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <iterator>
+
+using namespace std;
+int main(int argc,char* argv[]) {
+	if (argc!=4) {
+		cout<<"format:"<<endl;
+		cout<<"build-new [center file] [key file] [output]"<<endl;
+		exit(1);
+	}
+	string center_file=argv[1];
+	string in_file=argv[2];
+	string out_file=argv[3];
+	vector<float> raw_data;
+	ifstream fcenter(center_file.c_str());
+	ifstream fin(in_file.c_str());
+	ofstream fout(out_file.c_str());
+	string line;
+	while(getline(fcenter,line)) {
+		float a;
+		istringstream iss(line);
+		vector<string> tokens{istream_iterator<string>{iss},istream_iterator<string>{}};
+		for (unsigned i=0;i<tokens.size();i++) {
+			istringstream(tokens[i])>>a;
+			raw_data.push_back(a);
+		}
+	}
+	fcenter.close();
+	float *centers = new float[raw_data.size()];
+	for (unsigned i=0;i<raw_data.size();i++) {
+		centers[i]=raw_data[i];
+	}
+	vector<float> features_data;
+	while(getline(fin,line)) {
+		float a;
+		istringstream iss(line);
+		vector<string> tokens{istream_iterator<string>{iss},istream_iterator<string>{}};
+		for (unsigned i=0;i<tokens.size();i++) {
+			istringstream(tokens[i])>>a;
+			features_data.push_back(a);
+		}
+	}
+	fin.close();
+	float *features=new float[features_data.size()];
+	for (unsigned i=0;i<features_data.size();i++) {
+		features[i]=features_data[i];
+	}
+	float *speed;
+	IndexParameters indexPara;
+	indexPara.algorithm = KDTREE;
+	indexPara.checks = 2048;
+	indexPara.trees=8;
+	indexPara.target_precision = -1;
+	indexPara.build_weight=0.01;
+	indexPara.memory_weight=1;
+	FLANN_INDEX index=flann_build_index(centers,raw_data.size()/128,128,speed,&indexPara,0);
+	FLANNParameters findPara;
+	findPara.log_level=LOG_NONE;
+	findPara.log_destination=NULL;
+	findPara.random_seed=CENTERS_RANDOM;
+	int* result=new int[features_data.size()/128];
+	int neighbors=flann_find_nearest_neighbors_index(index,features,features_data.size()/128,result,1,1024,&findPara);
+	for (unsigned i=0;i<features_data.size()/128-1;i++) {
+		fout<<result[i]<<" ";	
+	}	
+	fout<<result[features_data.size()/128-1];
+	fout.close();
+}
